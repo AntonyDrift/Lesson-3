0dec24264407e9d7f97da96f37ae1e76d93f2f75
AntonyDrift <42956301+AntonyDrift@users.noreply.github.com>
2018-09-09 17:33:57 +0300

    Create Exercise4.java

---
+++ Exercise4.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] array = new int[4];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 10 + (int) (Math.random() * (99 - 10) + 1);
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
+
+
+
