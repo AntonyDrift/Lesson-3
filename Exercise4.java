60b3f8306cf9dde009b90f597536a8e28a0fa544
AntonyDrift <42956301+AntonyDrift@users.noreply.github.com>
2018-09-09 18:07:07 +0300

    Update Exercise4.java

--- Exercise4.java
+++ Exercise4.java
@@ -6,8 +6,17 @@ public class Main {
         int[] array = new int[4];
         for (int i = 0; i < array.length; i++) {
             array[i] = 10 + (int) (Math.random() * (99 - 10) + 1);
+            System.out.println(array[i] + " ");
         }
-        System.out.println(Arrays.toString(array));
+        
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                if (array[i - 1] >= array[i]) {
+                    System.out.println("false");
+                    break;
+                } else System.out.println("good");
+            }
+        }
     }
 }
 
