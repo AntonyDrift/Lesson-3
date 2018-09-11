0d677362a97b7ea1c9c061830dcf1fe47481ce86
AntonyDrift <42956301+AntonyDrift@users.noreply.github.com>
2018-09-11 21:40:28 +0300

    Update Exercise4.java

--- Exercise4.java
+++ Exercise4.java
@@ -6,19 +6,16 @@ public class Main {
         int[] array = new int[4];
         for (int i = 0; i < array.length; i++) {
             array[i] = 10 + (int) (Math.random() * (99 - 10) + 1);
-            System.out.println(array[i] + " ");
         }
-        
+        System.out.println(Arrays.toString(array));
         for (int i = 0; i < array.length; i++) {
             if (i > 0) {
                 if (array[i - 1] >= array[i]) {
-                    System.out.println("false");
+                    System.out.println("This is a regular array!");
                     break;
-                } else System.out.println("good");
+                } else
+                    System.out.println("The array in a strictly increasing sequence!");
             }
         }
     }
 }
-
-
-
