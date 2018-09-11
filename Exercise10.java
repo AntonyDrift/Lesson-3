a4e76f63868608718f91641b4b56d7c3d9c96751
AntonyDrift <42956301+AntonyDrift@users.noreply.github.com>
2018-09-11 21:12:46 +0300

    Update Exercise10.java

--- Exercise10.java
+++ Exercise10.java
@@ -1,22 +1,22 @@
+import java.math.BigInteger;
+
 public class Main {
 
     public static void main(String[] args) {
-        int n = 1;
-        int result = 1;
+        int n = 300;
+        BigInteger result = BigInteger.valueOf(1);
 
-        while ( n == 0) {
+        while (n == 0) {
             System.out.println(result);
             break;
         }
         while (n < 0) {
-            System.out.println("minus");
+            System.out.println("The number can't be negative!");
             break;
         }
-
         for (int i = 1; i <= n; i++) {
-            result *= i;
+            result = result.multiply(BigInteger.valueOf(i));
             System.out.println(result);
         }
     }
 }
-
